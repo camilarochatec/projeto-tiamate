a068b1953d926b3820748984121718ccaabac515
ALISSON CESAR ALBUQUERQUE NOGUEIRA <alissonnogueira0468@digitalcollege.internal>
2025-05-08 11:58:25 -0300

    instagram

---
+++ src/components/Instagram.jsx
@@ -0,0 +1,13 @@
+import instagramImg from "../assets/foto1.jpg"
+
+const Instagram = () => {
+    return ( 
+        <>
+            <section className="">
+                <img src={instagramImg} alt="" />
+            </section>
+        </>
+     );
+}
+ 
+export default Instagram;
