b2272a40e3ffb65d24417f9a7ad7efddf18a8f29
unknown <isaiasnunes6257@digitalcollege.internal>
2025-05-08 11:58:04 -0300

    video

---
+++ src/components/videoTiamate.jsx
@@ -0,0 +1,15 @@
+const videoTiamate = () => {
+    return (
+        <>
+        <section>
+            <h1>
+            Coisas boas acontecem àqueles
+            <br/>
+            que gostam de café
+            </h1>
+        </section>
+        </>
+    );
+}
+ 
+export default videoTiamate;
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -4,6 +4,7 @@ const Inicio = () => {
     return (
         <>
             <Sobre />
+            <videoTiamate />
         </>
     );
 }
