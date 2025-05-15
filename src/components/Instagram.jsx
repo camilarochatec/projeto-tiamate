3569fc334e8c16dadb022f5524838cd72ad2e712
ALISSON CESAR ALBUQUERQUE NOGUEIRA <alissonnogueira0468@digitalcollege.internal>
2025-05-15 11:05:55 -0300

    Instagram

--- src/components/Instagram.jsx
+++ src/components/Instagram.jsx
@@ -1,16 +1,16 @@
 import foto1 from "../assets/instagram-foto1.jpg"
-import foto2 from "../assets/instagram-foto2.jpg"
+import foto2 from "../assets/instagram-foto2.png"
 import foto3 from "../assets/instagram-foto3.jpg"
-import foto4 from "../assets/instagram-foto4.jpg"
-import foto5 from "../assets/instagram-foto5.jpg"
-import foto6 from "../assets/instagram-foto6.jpg"
+import foto4 from "../assets/instagram-foto4.png"
+import foto5 from "../assets/instagram-foto5.png"
+import foto6 from "../assets/instagram-foto6.png"
 import BotaoVejaMais from "./BotaoVejaMais"
 
 const Instagram = () => {
     return ( 
         <>
-            <section className="">
-                <div className="relative bottom-[-153px] px-[70px] xl:px-[135px] ">
+            <section className="relative">
+                <div className="p-4 lg:px-[70px] xl:px-[135px] ">
                     <div className="flex flex-col gap-[30px] lg:flex lg:flex-row lg:justify-between lg:align-center ">
                         <div className="flex flex-col items-center lg:flex lg:flex-col lg:items-start">
                             <h3 className="text-[15px] lg:text-[18px] text-[#3B2416]">Para você e seus amigos</h3>
@@ -29,7 +29,7 @@ const Instagram = () => {
                         <img src={foto6} alt="" className="w-full " />
                     </div>
                 </div>
-                <div className="w-full sm:h-[153px] bg-[#E7E1DF] -z-10">
+                <div className="w-full h-[70px] md:h-[130px] xl:h-[160px] bg-[#E7E1DF] -z-10 absolute bottom-0">
                 </div>
             </section>
             
