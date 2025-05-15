3869029b9364f462058faad59840aee2326dc90b
unknown <nagilapereira8896@digitalcollege.internal>
2025-05-15 11:34:53 -0300

    sobre

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -5,6 +5,7 @@ import Sobre from "../components/Sobre";
 const Inicio = () => {
     return (
         <>
+            <Sobre />
             <Depoimentos />
             <FormFranqueado />
         </>
