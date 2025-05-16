bdf7a7c748e097417f1efdc7adcb35dbe70adc0c
Marcos Soares <messtutors@gmail.com>
2025-05-16 17:03:07 -0300

    ajustando layout

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -2,11 +2,13 @@ import Instagram from "../components/Instagram";
 import Depoimentos from "../components/Depoimentos/Depoimentos";
 import FormFranqueado from "../components/FormFranqueado";
 import Sobre from "../components/Sobre";
+import Cardapio from "../components/Cardapio";
 
 const Inicio = () => {
     return (
         <>
-            <Sobre />
+            <Cardapio />
+            {/* <Sobre /> */}
             {/* <Instagram />
             <Depoimentos />
             <FormFranqueado /> */}
