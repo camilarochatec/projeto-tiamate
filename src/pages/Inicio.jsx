79542ab2aec75b6c77b9b08d26c9d94c7b1c9976
vitoriaAlbuquerque07 <vitoriam.albuquerque00@gmail.com>
2025-05-22 08:32:27 -0300

    merges

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -2,6 +2,7 @@ import Instagram from "../components/Instagram";
 import Depoimentos from "../components/Depoimentos/Depoimentos";
 import FormFranqueado from "../components/FormFranqueado";
 import VideoTiamate from "../components/videoTiamate";
+import Sobre from "../components/Sobre";
 
 const Inicio = () => {
     return (
