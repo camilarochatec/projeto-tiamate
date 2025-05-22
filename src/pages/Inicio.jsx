5d6d14f72badc6ba899898e1b67156bb83a268c0
ALISSON CESAR ALBUQUERQUE NOGUEIRA <alissonnogueira0468@digitalcollege.internal>
2025-05-22 10:15:15 -0300

    enviado para revisao

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -6,19 +6,21 @@ import QuemSomos from "../components/QuemSomos/index.jsx";
 import Cardapio from "../components/Cardapio";
 import VideoTiamate from "../components/videoTiamate";
 import NossasCafeterias from "../components/NossasCafeterias";
+import Noticias from "../components/Noticias.jsx";
 
 const Inicio = () => {
     return (
         <>
-            <Instagram />
-            {/* <Depoimentos />
-            <FormFranqueado /> */}
-            <NossasCafeterias />
-            <QuemSomos />
-            <Cardapio />
+            {/* <Instagram />
+            <Depoimentos /> */}
+            <Noticias />
+            {/* <FormFranqueado /> */}
+            {/* <NossasCafeterias /> */}
+            {/* <QuemSomos />
+            <Cardapio /> */}
             {/* <Sobre /> */}
-            <VideoTiamate />
-            <Sobre />
+            {/* <VideoTiamate />
+            <Sobre /> */}
             {/* <Instagram />
             <Depoimentos />
             <FormFranqueado /> */}
