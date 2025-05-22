115769deeaa1084cc5fb20a2e52ed64ed9e976c1
freitas14.freitas@gmail.com <freitas14.freitas@gmail.com>
2025-05-22 09:02:01 -0300

    arquivos inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import Paths from "./routes/Paths";
+import { BrowserRouter } from "react-router-dom"; // ✅ Import necessário
 import { queryClient } from "./services";
 import 'boxicons'
 
