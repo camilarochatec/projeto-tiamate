e4487e93bdd7779aced799e5e6e72bf5b7373d2d
isaiassn7 <isaiassousa19092000@gmail.com>
2025-05-25 13:18:49 -0300

    responsivo celular pronto

--- src/components/FormContato.jsx
+++ src/components/FormContato.jsx
@@ -8,7 +8,7 @@ import linkedin from "../assets/linkedin-icon.png"
 const FormContato = () => {
     return (
         <>
-            <section>
+            <section className="flex flex-col gap-[30px] my-[30px]">
                 <div className="flex flex-col items-center">
 
                     <h1 className="text-[47px] text-[#A97744] font-bold">Alguma dúvida?</h1>
@@ -16,13 +16,13 @@ const FormContato = () => {
 
                     <form className="flex flex-col gap-[10px]">
                         <div className="flex gap-[10px] items-center w-[370px] h-[60px] rounded-[5px] bg-[#E7E1DF]">
-                            <div className="flex justify-center items-center h-[32px] w-[32px] rounded-full ml-[10px] bg-[#969696]">
+                            <div className="flex justify-center items-center h-[33px] w-[33px] rounded-full ml-[10px] bg-[#969696]">
                                 <img className="w-[18px] h-[18px]" src={user} alt="user" />
                             </div>
                             <input className="outline-none" type="text" placeholder="Nome e Sobrenome" />
                         </div>
                         <div className="flex gap-[10px] items-center w-[370px] h-[60px] rounded-[5px] bg-[#E7E1DF]">
-                            <div className="flex justify-center items-center h-[32px] w-[32px] rounded-full ml-[10px] bg-[#969696]">
+                            <div className="flex justify-center items-center h-[33px] w-[33px] rounded-full ml-[10px] bg-[#969696]">
                                 <img src={email} alt="email" />
                             </div>
                             <input className="outline-none" type="text" placeholder="email@exemplo.com" />
@@ -34,14 +34,14 @@ const FormContato = () => {
                     </form>
 
                 </div>
-                <div>
-                    <div>
+                <div className="flex flex-col items-center gap-[30px]">
+                    <div className="flex justify-between w-[310px]">
                         <img src={facebook} alt="facebook" />
                         <img src={instagram} alt="instagram" />
                         <img src={youtube} alt="youtube" />
                         <img src={linkedin} alt="linkedin" />
                     </div>
-                    <h1>Curta e<br />Comparilhe</h1>
+                    <h1 className="text-[30px] text-[#3B2416] font-bold">Curta e Comparilhe</h1>
                 </div>
             </section>
         </>
