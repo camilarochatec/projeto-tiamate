023e021c1e31e20563614c12b746110bd3f307b1
Rafael <fconrafael@gmail.com>
2025-05-28 20:11:43 -0300

    banner endereços

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -2,16 +2,26 @@ import Instagram from "../components/Instagram";
 import Depoimentos from "../components/Depoimentos/Depoimentos";
 import FormFranqueado from "../components/FormFranqueado";
 import Sobre from "../components/Sobre";
-import Valores from "../components/Valores";
 import QuemSomos from "../components/QuemSomos/index.jsx";
 import Cardapio from "../components/Cardapio";
 import VideoTiamate from "../components/videoTiamate";
 import NossasCafeterias from "../components/NossasCafeterias";
+import Enderecos from "../components/Enderecos/index.jsx";
 
 const Inicio = () => {
     return (
         <>
-            <Valores/>
+        <Enderecos/>
+            {/* <Instagram />
+            <FormFranqueado />
+            <NossasCafeterias />
+            <QuemSomos />
+            <Cardapio />
+            <Sobre />
+            <VideoTiamate />
+            <Sobre />
+            <Depoimentos />
+            <FormFranqueado /> */}
         </>
     );
 }
