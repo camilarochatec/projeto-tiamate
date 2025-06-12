fc393c148c4fac647d062d86ddd1025c0720c3db
Rafael <fconrafael@gmail.com>
2025-06-12 08:47:23 -0300

    Update index.jsx

--- src/components/Enderecos/index.jsx
+++ src/components/Enderecos/index.jsx
@@ -28,29 +28,7 @@ const Enderecos = () => {
             <div className="grid lg:grid-cols-3 gap-24 py-20 leading-tight">
                 {
                     loading ? (
-                        <div className="flex items-center justify-center text-marrom text-lg">
-                            <svg
-                                className="animate-spin h-5 w-5 mr-2 text-marrom"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                            >
-                                <circle
-                                    className="opacity-25"
-                                    cx="12"
-                                    cy="12"
-                                    r="10"
-                                    stroke="currentColor"
-                                    strokeWidth="4"
-                                ></circle>
-                                <path
-                                    className="opacity-75"
-                                    fill="currentColor"
-                                    d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
-                                ></path>
-                            </svg>
-                            Carregando...
-                        </div>
+                        <div>Carregando</div>
                     ) : enderecos.map((endereco) => (
                         <div className="max-w-sm text-center lg:text-left ">
                             <div className="mb-2">
