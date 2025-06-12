d47650ff82c91ac0a8efd96dd1f1cc71167152ac
Rafael <fconrafael@gmail.com>
2025-06-11 22:36:32 -0300

    update parcial page noticia e hooks depoimentos

--- src/components/Enderecos/index.jsx
+++ src/components/Enderecos/index.jsx
@@ -7,7 +7,7 @@ import { useEffect, useState } from "react";
 
 const Enderecos = () => {
     const [enderecos, setEnderecos] = useState();
-    const [loadind, setLoading] = useState(true);
+    const [loading, setLoading] = useState(true);
     async function buscarEnderecos() {
         const request = await AXIOS.get("/enderecos")
         if (request.data) {
@@ -27,8 +27,30 @@ const Enderecos = () => {
 
             <div className="grid lg:grid-cols-3 gap-24 py-20 leading-tight">
                 {
-                    loadind ? (
-                        <div>Carregando</div>
+                    loading ? (
+                        <div className="flex items-center justify-center text-marrom text-lg">
+                            <svg
+                                className="animate-spin h-5 w-5 mr-2 text-marrom"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                            >
+                                <circle
+                                    className="opacity-25"
+                                    cx="12"
+                                    cy="12"
+                                    r="10"
+                                    stroke="currentColor"
+                                    strokeWidth="4"
+                                ></circle>
+                                <path
+                                    className="opacity-75"
+                                    fill="currentColor"
+                                    d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+                                ></path>
+                            </svg>
+                            Carregando...
+                        </div>
                     ) : enderecos.map((endereco) => (
                         <div className="max-w-sm text-center lg:text-left ">
                             <div className="mb-2">
--- src/pages/Noticias.jsx
+++ src/pages/Noticias.jsx
@@ -1,20 +1,31 @@
-import { Link } from "react-router";
+import Banner from "../components/Banner";
+import bannerImg from "../assets/banner_01 1.png";
+import QuemSomos from "../components/QuemSomos";
+import VideoTiamate from "../components/videoTiamate";
+import FormFranqueado from "../components/FormFranqueado";
+import Modelo from "../components/Modelo";
+import ModelosFranquia from "../components/ModelosFranquia";
+import Diferenciais from "../components/Diferenciais";
+
 
 const Noticias = () => {
     return (
         <>
-            <h1>Pagina de noticias</h1>
-            <ul>
-                <li>
-                    <Link to={"/noticias/1"}>Noticia 1</Link>
-                </li>
-                <li>
-                    <Link to={"/noticias/2"}>Noticia 2</Link>
-                </li>
-                <li>
-                    <Link to={"/noticias/3"}>Noticia 3</Link>
-                </li>
-            </ul>
+            <Banner
+                imagem={bannerImg}
+                texto={["Um café quentinho", "aqui na Tiamate", "muda o dia."]}
+                redesSociais
+                fraseColorida // Se a primeira frase deve ser colorida ou não
+                corDaFrase={{ color: "text-mostarda" }} // Aqui conseguimos escolher a cor que vai estar na primeira frase
+            // textoAdireita
+            />
+            <QuemSomos/>
+            <VideoTiamate/>
+            <Modelo/>
+            <ModelosFranquia/>
+            <Diferenciais/>
+            <FormFranqueado/>
+
         </>
     );
 }
