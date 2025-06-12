7bbb680e395e9d56774c0c562bd4f3fc67a1a082
isaiassn7 <isaiassousa19092000@gmail.com>
2025-06-11 21:55:03 -0300

    contatopage

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -1,9 +1,16 @@
+import { Header } from "antd/es/layout/layout";
 import Enderecos from "../components/Enderecos";
+import Banner from "../components/Banner";
+import NossasCafeterias from "../components/NossasCafeterias";
+import FormContato from "../components/FormContato";
 
 const Contato = () => {
     return (
         <>
+            <Header />
+            <NossasCafeterias />
             <Enderecos/>
+            <FormContato />
         </>
     );
 }
