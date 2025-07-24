c0d1d37c24a7d4843272f9c7b821c9460ff49aad
eduardo.freitas <eduardo.freitas@semace.ce.gov.br>
2025-07-24 08:59:55 -0300

    Adicionado as pictures direto do banco

--- src/components/NossasCafeterias.jsx
+++ src/components/NossasCafeterias.jsx
@@ -1,39 +1,15 @@
 import React, { useState } from 'react';
 import BotaoVejaMais from "./BotaoVejaMais";
-import imagem1 from "../assets/cafeterias-foto1.png"
-import imagem2 from "../assets/cafeterias-foto2.png"
-import imagem3 from "../assets/cafeterias-foto3.png"
-import imagem4 from "../assets/cafeterias-foto4.png"
-import imagem5 from "../assets/cafeterias-foto5.png"
+import { useBuscarPictures } from '../hooks/picturesHooks'; 
 
 
 
 const NossasCafeterias = () => {
+  const { data: imagensCafeterias = [] } = useBuscarPictures();
+
   const [indiceAtivo, setIndiceAtivo] = useState(0);
   const [imagemAmpliada, setImagemAmpliada] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const imagensCafeterias = [
-    imagem1,
-    imagem2,
-    imagem3,
-    imagem4,
-    imagem5,
-  ];
-
-  const imagensCafeterias1 = [
-    imagem1,
-    imagem2,
-    imagem3,
-    imagem4,
-  ];
-
-  const imagensCafeterias2 = [
-    imagem1,
-    imagem2,
-    imagem3,
-  ];
-
 
   const handleBolinhaHover = (indice) => {
     setIndiceAtivo(indice);
@@ -48,7 +24,7 @@ const NossasCafeterias = () => {
     setIsModalOpen(false);
     setTimeout(() => {
       setImagemAmpliada(null);
-    }, 300); // Deve ser igual ou maior que a duration-300ms do Tailwind
+    }, 300);
   };
 
   return (
@@ -58,98 +34,42 @@ const NossasCafeterias = () => {
         <BotaoVejaMais />
       </div>
 
-      <div>
-        <div className="hidden xl:block xl:flex xl:justify-between">
-          {imagensCafeterias.map((imagem, index) => (
-            <img
-              key={index}
-              src={imagem}
-              alt={`Cafeteria ${index + 1}`}
-              className={`
-                hover:cursor-pointer transform transition-all duration-200
-                ${index === indiceAtivo ? 'scale-105' : 'scale-100'}
-              `}
-              onMouseEnter={() => handleBolinhaHover(index)}
-              onClick={() => handleImageClick(imagem)}
-            />
-          ))}
-        </div>
-
-        <div className="hidden lg:block xl:hidden lg:flex lg:justify-between">
-          {imagensCafeterias1.map((imagem, index) => (
-            <img
-              key={index}
-              src={imagem}
-              alt={`Cafeteria ${index + 1}`}
-              className={`
-                hover:cursor-pointer transform transition-all duration-200
-                ${index === indiceAtivo ? 'scale-105' : 'scale-100'}
-              `}
-              onMouseEnter={() => handleBolinhaHover(index)}
-              onClick={() => handleImageClick(imagem)}
-            />
-          ))}
-        </div>
-
-        <div className="hidden md:block lg:hidden xl:hidden md:flex md:justify-between">
-          {imagensCafeterias2.map((imagem, index) => (
-            <img
-              key={index}
-              src={imagem}
-              alt={`Cafeteria ${index + 1}`}
-              className={`
-                hover:cursor-pointer transform transition-all duration-200
-                ${index === indiceAtivo ? 'scale-105' : 'scale-100'}
-              `}
-              onMouseEnter={() => handleBolinhaHover(index)}
-              onClick={() => handleImageClick(imagem)}
-            />
-          ))}
-        </div>
-        
-        <div className="grid grid-cols-2 sm:hidden">
-            <img src={imagem1} alt="" className="hover:cursor-pointer transform transition-all duration-200 hover:scale-105" />
-            <img src={imagem2} alt="" className="hover:cursor-pointer transform transition-all duration-200 hover:scale-105" />
-            <img src={imagem3} alt="" className="sm:hidden md:block hover:cursor-pointer transform transition-all duration-200 hover:scale-105" />
-            <img src={imagem4} alt="" className="sm:hidden lg:block hover:cursor-pointer transform transition-all duration-200 hover:scale-105" />
-            <img src={imagem5} alt="" className="hidden xl:block hover:cursor-pointer transform transition-all duration-200 hover:scale-105" />
-        </div>
-        
-        <div className="flex justify-center gap-[15px] p-4 xl:p-12">
-          {imagensCafeterias.map((_, index) => (
-            <div
-              key={index}
-              className={`
-                rounded-full bg-cinza-escuro hover:cursor-pointer transform transition-all duration-200
-                w-[15px] h-[15px]
-                ${index === indiceAtivo ? 'scale-125 bg-bege-escuro' : 'scale-100'}
-              `}
-              onMouseEnter={() => handleBolinhaHover(index)}
-              onClick={() => handleImageClick(imagensCafeterias[index])}
-            ></div>
-          ))}
-        </div>
+      <div className="flex flex-wrap justify-center gap-4">
+        {imagensCafeterias.map((img, index) => (
+          <img
+            key={index}
+            src={img.picture_imagem}
+            alt={img.picture_nome}
+            className={`
+              hover:cursor-pointer transform transition-all duration-200
+              ${index === indiceAtivo ? 'scale-105' : 'scale-100'}
+              w-[200px] h-[200px] object-cover
+            `}
+            onMouseEnter={() => handleBolinhaHover(index)}
+            onClick={() => handleImageClick(img.picture_imagem)}
+          />
+        ))}
       </div>
 
-      {/* Modal da Imagem Ampliada */}
-      {imagemAmpliada && (
-        <div
-          className={`
-            fixed inset-0 z-50 cursor-pointer
-            flex justify-center items-center
-            transition-opacity duration-300 ease-in-out
-            ${isModalOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}
-          `}
-          onClick={handleCloseAmpliada}
-        >
-          {/* Fundo escuro semi-transparente que fará a transição de opacidade */}
-          <div className={`
-            absolute inset-0 bg-black
-            transition-opacity duration-300 ease-in-out
-            ${isModalOpen ? 'opacity-80' : 'opacity-0'}
-          `}></div>
+      <div className="flex justify-center gap-[15px] p-4 xl:p-12">
+        {imagensCafeterias.map((_, index) => (
+          <div
+            key={index}
+            className={`
+              rounded-full bg-cinza-escuro hover:cursor-pointer transform transition-all duration-200
+              w-[15px] h-[15px]
+              ${index === indiceAtivo ? 'scale-125 bg-bege-escuro' : 'scale-100'}
+            `}
+            onMouseEnter={() => handleBolinhaHover(index)}
+            onClick={() => handleImageClick(imagensCafeterias[index].picture_imagem)}
+          ></div>
+        ))}
+      </div>
 
-          {/* A imagem em si, que terá opacidade total */}
+      {/* Modal */}
+      {imagemAmpliada && (
+        <div className={`fixed inset-0 z-50 cursor-pointer flex justify-center items-center transition-opacity duration-300 ease-in-out ${isModalOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`} onClick={handleCloseAmpliada}>
+          <div className={`absolute inset-0 bg-black transition-opacity duration-300 ease-in-out ${isModalOpen ? 'opacity-80' : 'opacity-0'}`}></div>
           <img
             src={imagemAmpliada}
             alt="Imagem Ampliada"
@@ -161,5 +81,6 @@ const NossasCafeterias = () => {
     </section>
   );
 };
- 
+
 export default NossasCafeterias;
+ 
